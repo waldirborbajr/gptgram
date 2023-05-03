8c6d14ff3420ab888bb63a87ab278ab3208d9dd1
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-05-03 08:43:34 -0300

    chore(update): tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: "jit",
+  // purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
