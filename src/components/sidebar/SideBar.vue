d79539cf1a7507232be932c7df5d4e86b9f38e72
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-05-05 12:06:04 -0300

    chore(update): sidebar
    Signed-off-by: Waldir BORBA JUNIOR <wborbajr@gmail.com>

--- src/components/sidebar/SideBar.vue
+++ src/components/sidebar/SideBar.vue
@@ -1,7 +1,130 @@
 <script setup></script>
 
 <template>
-  <div class="">SideBar</div>
+  <!-- container -->
+  <div class="relative flex min-h-screen">
+    <!-- sidebar -->
+    <div class="bg-slate-700 text-slate-300 w-64 p-2">
+      <a href="/" class="flex items-center py-3 space-x-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+          /></svg
+        ><span class="text-slate-300">B+ Tech·​nol·​o·​gy</span>
+      </a>
+      <nav>
+        <a href="/" class="base-menu"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
+          </svg>
+          <span class="text-slate-200">Home</span>
+        </a>
+        <a href="/" class="base-menu"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
+            />
+          </svg>
+          <span class="text-slate-200">Chat</span>
+        </a>
+        <a href="/" class="base-menu"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span class="base-text">Time</span>
+        </a>
+        <a href="/" class="base-menu"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5"
+            />
+          </svg>
+          <span class="base-text">Alarm</span>
+        </a>
+        <a href="/" class="base-menu"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+            />
+          </svg>
+          <span class="base-text">Bookmark</span>
+        </a>
+      </nav>
+    </div>
+    <!-- main content -->
+    <div class="flex-1">
+      <!-- header -->
+      <div class="bg-slate-500 shadow px-2 py-4">header</div>
+      <!-- content -->
+      <div class="p-8 text-slate-600">content</div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.base-menu {
+  @apply flex items-center py-3 space-x-2 hover:bg-slate-500 rounded-md hover:text-slate-700;
+}
+
+.base-text {
+  @apply text-slate-200 hover:text-slate-700;
+}
+</style>
