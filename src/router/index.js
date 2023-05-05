dfc3ec45bbe5528f0824e969b09c56a03c3534ae
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-05-05 18:24:44 -0300

    chore(update): fix content menu open position
    Signed-off-by: Waldir BORBA JUNIOR <wborbajr@gmail.com>

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,30 @@ import { createRouter, createWebHistory } from "vue-router";
 import DashView from "../views/DashView.vue";
 import GptView from "../views/GptView.vue";
 import ProfileView from "../views/ProfileView.vue";
+import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
+    name: "dash",
     component: DashView,
-  },
-  {
-    path: "/gpt",
-    name: "gpt",
-    component: GptView,
-  },
-  {
-    path: "/profile",
-    name: "gpt",
-    component: ProfileView,
+    children: [
+      {
+        path: "/home",
+        name: "home",
+        component: HomeView,
+      },
+      {
+        path: "/gpt",
+        name: "gpt",
+        component: GptView,
+      },
+      {
+        path: "/profile",
+        name: "profile",
+        component: ProfileView,
+      },
+    ],
   },
 ];
 
