52fd21f7a6fdf84149c21894f2fde88d5a712bda
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-05-05 18:12:04 -0300

    chore(fix): remove unused reference
    Signed-off-by: Waldir BORBA JUNIOR <wborbajr@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   mode: "jit",
-  // purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
